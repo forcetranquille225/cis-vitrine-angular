95fa611c0f075af3937455d02ffda82d95bef1f6
forcetranquille225 <bmamadoualpha266@gmail.com>
2025-10-22 09:42:20 +0000

    Fix : layouts content needed

---
+++ src/app/layouts/visitor-layout/visitor-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visitor-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './visitor-layout.component.html',
+  styleUrl: './visitor-layout.component.scss'
+})
+export class VisitorLayoutComponent {
+
+}
