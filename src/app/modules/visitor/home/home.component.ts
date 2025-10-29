01d6aafa5d58d50f2b0935f3c647a45d39e10462
forcetranquille225 <bmamadoualpha266@gmail.com>
2025-10-29 19:14:38 +0000

    FIX : add the content into the home page

--- src/app/modules/visitor/home/home.component.ts
+++ src/app/modules/visitor/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  // Navigation vers les services
+  navigateToServices() {
+    // TODO: Implémenter la navigation vers la section services
+  }
 
+  // Navigation vers la page de contact
+  navigateToContact() {
+    // TODO: Implémenter la navigation vers la page de contact
+  }
 }
